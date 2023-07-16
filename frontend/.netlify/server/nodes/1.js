743dafbcd80b33b7b15bf7d2b7db3f86fb460196
santhana krishnan <29514440+hashkrish@users.noreply.github.com>
2023-07-16 10:11:48 +0530

    ignore .netlify

--- frontend/.netlify/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.ccac05ff.js","_app/immutable/chunks/index.568a4140.js","_app/immutable/chunks/singletons.cfcb0119.js"];
-export const stylesheets = [];
-export const fonts = [];
