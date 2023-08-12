b5e937ca47c1fc447a1ae312fdc9c3ff8f5590ef
santhana krishnan <29514440+hashkrish@users.noreply.github.com>
2023-08-13 02:25:40 +0530

    add svelte aliases

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -11,32 +11,32 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	},
-	alias: {
-		$api: './src/api',
-		$assets: './src/assets',
-		$components: './src/components',
-		$config: './src/config',
-		$constants: './src/constants',
-		$controllers: './src/controllers',
-		$directives: './src/directives',
-		$generated: './src/generated',
-		$graphql: './src/graphql',
-		$hooks: './src/hooks',
-		$layouts: './src/routes/layouts',
-		$lib: './src/lib',
-		$middlewares: './src/middlewares',
-		$pages: './src/routes/pages',
-		$repositories: './src/repositories',
-		$routes: './src/routes',
-		$services: './src/services',
-		$src: './src',
-		$stores: './src/stores',
-		$styles: './src/styles',
-		$types: './src/types',
-		$utils: './src/utils',
-		$validators: './src/validators'
+		adapter: adapter(),
+		alias: {
+			$api: './src/api',
+			$assets: './src/assets',
+			$components: './src/components',
+			$config: './src/config',
+			$constants: './src/constants',
+			$controllers: './src/controllers',
+			$directives: './src/directives',
+			$generated: './src/generated',
+			$graphql: './src/graphql',
+			$hooks: './src/hooks',
+			$layouts: './src/routes/layouts',
+			$lib: './src/lib',
+			$middlewares: './src/middlewares',
+			$pages: './src/routes/pages',
+			$repositories: './src/repositories',
+			$routes: './src/routes',
+			$services: './src/services',
+			$src: './src',
+			$stores: './src/stores',
+			$styles: './src/styles',
+			$types: './src/types',
+			$utils: './src/utils',
+			$validators: './src/validators'
+		}
 	}
 };
 
